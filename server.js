ad916ee9caafc0c8fbf0d2bdc5fdbf0a5368bda5
Joseph Weathers <weathersja2000@gmail.com>
2026-01-17 19:30:32 -0500

    Initial TrendForge trend service

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ import { buildEditorial } from "./editorial/buildEditorial.js";
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "2mb" }));
 
 // Base44 function to compute topics from TrendItems already stored for a TrendRun
 const BASE44_BUILD_TOPICS_URL =
